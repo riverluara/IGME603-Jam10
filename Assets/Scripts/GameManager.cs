faca7653e11197880a60584577e01de5d862e5d3
Emily Dixon <emilyirishdixon@gmail.com>
2019-03-24 13:11:31 -0400

    Player Base, Command System, and Network Base Added
    Basic player class created, just holds the data needed for the player to function in combat at this point. File I/O for reading in the list of commands each job can use, network base set up for when that needs to happen. Next steps on my end are fleshing out reading in input from the board to check against the command list, and then doing actions against that, which will finish up combat on the player end, and then sort out how were gonna have our second player in the end.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Player;
+    private Player player;
+    private int job;
+    public string[] lines;
+    public List<string> commands;
+
+
+    void Start()
+    {
+        player = Player.GetComponent<Player>();
+        job = player.GetJob();
+        ReadFromFile();
+        MakeValid();
+    }
+
+    void ReadFromFile()
+    {
+        StreamReader inStream = new StreamReader("Assets/Files/Commands.txt");
+        string file = inStream.ReadToEnd();
+        inStream.Close();
+        lines = file.Split("\n"[0]);
+    }
+
+    void MakeValid()
+    {
+        string inputs = lines[job];
+        string[] temp = inputs.Split(" "[0]);
+        commands = new List<string>(temp);
+        commands.RemoveAt(0);
+    }
+}
