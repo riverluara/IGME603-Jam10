281063900967d0745a8878884e8320b848cceb62
Yun Jiang (RIT Student) <yj9812@ad.rit.edu>
2019-03-25 17:53:02 -0400

    merge AI

---
+++ Assets/Scripts/WordSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordSpawner : MonoBehaviour
+{
+    public GameObject wordPrefab;
+    public WordDisplay SpawnWord()
+    {
+
+        GameObject wordObj = Instantiate(wordPrefab);//Add Random Position later
+        WordDisplay wordDisplay = wordObj.GetComponentInChildren<WordDisplay>();
+
+        return wordDisplay;
+    }
+}
