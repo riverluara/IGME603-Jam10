c8cb0dc6b71c52baed18c4e640585c204208816a
DIGITALSTORM-PC\Admin <jxw7456@g.rit.edu>
2019-03-25 11:24:01 -0400

    creating scene changing layout

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+// Manages scene changing throughout the game
+public class SceneControl : MonoBehaviour
+{
+    public string sceneName;
+    public Image img;
+    public Animator anim;
+
+    // Loading level
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Restarting Level
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    // Fade in-between scenes
+    IEnumerator Fading()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => img.color.a == 1);
+        LoadScene();
+    }
+}
