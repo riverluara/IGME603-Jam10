355c747c273f184fbf2808898a9ec58edadf2027
Freddie <masterfreddiewu@gmail.com>
2019-03-26 01:19:34 -0400

    basic overworld added.

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Node : MonoBehaviour {
+    public string scenePath;
+    private bool isLocked;
+    private bool isFinished;
+
+    // Start is called before the first frame update
+    void Start() {
+        isLocked = false;
+        isFinished = false;
+    }
+
+    public bool IsLocked{
+        get { return this.isLocked; }
+        set { this.isLocked = value; }
+    }
+
+    public bool IsFinished{
+        get { return this.isFinished; }
+        set { this.isFinished = value; }
+    }
+
+    public void switchScene(){
+        Debug.Log("SwitchScene");
+        if (!isLocked & !isFinished) SceneManager.LoadScene("test");
+        SceneManager.LoadScene("test");
+    }
+
+    // public void save(){
+    //     saveNodeData();
+    // }
+
+    // static void saveNodeData(){
+    //         PlayerPrefs.SetString(gameObject.name, gameObject.name);
+    //         PlayerPrefs.SetInt(gameObject.name + "isFinished", Convert.ToInt32(isFinished));
+    //         PlayerPrefs.SetInt(gameObject.name + "isLocked", Convert.ToInt32(isLocked)); 
+    //         PlayerPrefs.Save();
+    // }
+}
---
+++ Assets/Scripts/NodeManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class NodeManager : MonoBehaviour {
+    public GameObject[] _nodes;
+    public static GameObject[] nodes;
+    private static NodeManager nodeManager = null;
+    public static NodeManager getInstance() { return nodeManager; }
+
+    void Start(){
+        if (nodeManager == null) nodeManager = this;
+        nodes = _nodes;
+        if (nodes.Length != 0){
+            bool isSaved = PlayerPrefs.HasKey(nodes[0].name);
+
+            if(isSaved){ loadNodeData(); }
+            else{ foreach(GameObject node in nodes){ saveNodeData(node); }}
+        }
+        else { Debug.Log("No node");}
+    }
+
+    static void saveNodeData(GameObject node){
+        Node n = node.GetComponent<Node>();
+
+        if (n != null) {
+            PlayerPrefs.SetString(node.name, node.name);
+            PlayerPrefs.SetInt(node.name + "isFinished", Convert.ToInt32(n.IsFinished));
+            PlayerPrefs.SetInt(node.name + "isLocked", Convert.ToInt32(n.IsLocked));
+        }  
+    }
+
+    static void loadNodeData(){
+        foreach(GameObject node in nodes){
+            Node n = node.GetComponent<Node>();
+
+            n.IsFinished = PlayerPrefs.GetInt(node.name + "isFinished") != 0;
+            n.IsLocked = PlayerPrefs.GetInt(node.name + "isLocked") != 0;
+        }
+    }
+
+    public void loadScene(GameObject node){
+        Node n = node.GetComponent<Node>();
+        n.switchScene();
+    }
+
+
+}
