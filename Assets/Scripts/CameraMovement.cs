655591f9d745f6e0d79221b976f69f02b9c24a80
riverluara <riverphoebe@163.com>
2019-04-22 22:54:02 -0400

    add scene for trailar

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float target = 209.5f;
+    public float speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.z < target)
+        {
+            this.transform.position += Vector3.forward * speed;
+        }
+        
+    }
+}
